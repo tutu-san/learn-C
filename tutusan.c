e42ef47e5a8bdcb5ac3e33ffdef63058d94ded80
tutu_san <tumuri4071.dev@gmail.com>
2022-06-08 18:25:17 +0900

    chenge tutusan.c for Q11-01

--- tutusan.c
+++ tutusan.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+int sum(void); //これが、プロトタイプ宣言.
+
 int main(void){
-    printf("Hello Tutusan.");
+    sum(); //呼び出し部分。
+    return 0;
+}
+
+int sum(void){
+    printf("%d\n", (1 + 100 ) * 100 /2 );
     return 0;
 }
