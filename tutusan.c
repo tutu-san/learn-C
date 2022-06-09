f61afa825312b2e4ef79b4160b398f4535e6421d
tutu_san <tumuri4071.dev@gmail.com>
2022-06-09 17:37:14 +0900

    tutusan.c changed for  11-3

--- tutusan.c
+++ tutusan.c
@@ -3,11 +3,14 @@
 int sum(int, int); //これが、プロトタイプ宣言.   
 
 int main(void){
-    sum(50, 100); //呼び出し部分。
+    int value;
+    value = sum(50, 100);
+    printf("%d\n", value);
     return 0;
 }
 
 int sum(int min, int max){
-    printf("%d\n", (min + max ) * (max - min + 1) /2 );
-    return 0;
+    int num;
+    num = ( min + max ) * (max - min + 1) / 2;
+    return num;
 }
