25baf1c61b8cfe164ca1726a69c025268549a971
tutu_san <tumuri4071.dev@gmail.com>
2022-06-13 18:10:04 +0900

    change code tutusan.c for 12-02-3 ?

--- tutusan.c
+++ tutusan.c
@@ -5,10 +5,14 @@ int count; //グローバルな変数
 int countfunc(void);
 
 int main(void){
+    
+    int count; //ここで、ローカル関数として宣言。
+    
     countfunc();
     count = 10; //突然変更した。
     countfunc();
     countfunc();
+    printf(" main : count = %d\n ", count);
     return 0;
 }
 
