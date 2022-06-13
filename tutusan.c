6168226424a80f2e73c56c3a5414162392b184c1
tutu_san <tumuri4071.dev@gmail.com>
2022-06-13 17:30:15 +0900

    change code tutusan.c for 12-01-4

--- tutusan.c
+++ tutusan.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-int countfunc(void);
-
 int main(void){
-    int count = 0;
-    countfunc();
-    countfunc();
-    countfunc();
-    return 0;
-}
+    int value1 = 10;
+    int value2 = 20;
 
-int countfunc(void){
-    int count; //èâä˙âªÇµÇ»Ç¢ÅB
-    count ++;
-    printf("%d\n",count);
-    return count;
+    printf(" 1:value1 %d\n", value1);
+    printf(" 1:value2 %d\n", value2);
+
+    {
+        int value1;
+        value1 = 30;
+        value2 = 40;
+        printf(" 2:value1 %d\n", value1);
+        printf(" 2:value2 %d\n", value2);
+    }
+
+    printf(" 3:value1 %d\n", value1);
+    printf(" 3:value2 %d\n", value2);
+
+    return 0;
 }
