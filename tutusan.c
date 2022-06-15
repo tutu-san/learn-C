ae92274c0ca72906bff11ce9589cbfb45f80c1c8
tutu_san <tumuri4071.dev@gmail.com>
2022-06-15 18:16:14 +0900

    change code tutusan.c for 13-02-2

--- tutusan.c
+++ tutusan.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    int array[] = { 42, 79, 13, 75, 19};
+    int array[] = { 42, 79, 13, 75, 19 };
     int i;
 
-    for( i = 0; i < 5; i++)
+    for ( i = 0; i < sizeof(array) / sizeof(array[0]); i++)
     {
-        printf("array[%d] = %d\n", i ,array[i]);
+        printf("array[%d] = %d\n", i, array[i]);
     }
 
     return 0;
