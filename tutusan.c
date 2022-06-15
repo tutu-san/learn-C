ba6532df95f8da4e4e24ca666b13328dbae21a7b
tutu_san <tumuri4071.dev@gmail.com>
2022-06-15 18:26:51 +0900

    change code tutusan.c for 13-02-2 unfinshed

--- tutusan.c
+++ tutusan.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-    int array[] = { 42, 79, 13, 75, 19 };
+    int array1[] = { 42, 79, 13, 19, 41};
+    int array2[] = { 1, 2, 3, 4, 5 };
     int i;
 
-    for ( i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    for( i = 0; i < sizeof(array2)/ sizeof(array2[0]); i++)
     {
-        printf("array[%d] = %d\n", i, array[i]);
+        printf("array2[%d] = %d\n", i, array2[i]);
     }
 
-    return 0;
+    //array1を同2にコピーしよう。
 }
