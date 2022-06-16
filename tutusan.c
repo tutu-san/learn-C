73ebe42fcafcb6fa29dfbf9be136db3681457cf7
tutu_san <tumuri4071.dev@gmail.com>
2022-06-16 19:32:51 +0900

    change code tutusan.c for 13-02-4

--- tutusan.c
+++ tutusan.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
+#include <memory.h>
 
-int main(void)
-{
-    int array1[] = { 42, 79, 13, 19, 41};
+int main(void){
+    int array1[] = { 42, 79, 13, 19, 41 };
     int array2[] = { 1, 2, 3, 4, 5 };
     int i;
 
-    for( i = 0; i < sizeof(array2)/ sizeof(array2[0]); i++) //初期値表示
-    {
+    for ( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++ ){
         printf("array2[%d] = %d\n", i, array2[i]);
     }
 
-    //array1を同2にコピーしよう。
-    for( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){   //コピー
-       array2[i] = array1[i]; 
-    }
+    memcpy( array2, array1, sizeof(array1)); //array1をarray2にコピー
 
-    for( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){   //数値表示
+    for ( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){
         printf("array2[%d] = %d\n", i, array2[i]);
     }
-
+    
     return 0;
 }
