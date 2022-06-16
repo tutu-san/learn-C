6801101b416ea8adea195390af950d9898210875
tutu_san <tumuri4071.dev@gmail.com>
2022-06-16 18:11:04 +0900

    change code tutusan.c for 13-02-3 finshed ver.

--- tutusan.c
+++ tutusan.c
@@ -6,10 +6,19 @@ int main(void)
     int array2[] = { 1, 2, 3, 4, 5 };
     int i;
 
-    for( i = 0; i < sizeof(array2)/ sizeof(array2[0]); i++)
+    for( i = 0; i < sizeof(array2)/ sizeof(array2[0]); i++) //初期値表示
     {
         printf("array2[%d] = %d\n", i, array2[i]);
     }
 
     //array1を同2にコピーしよう。
+    for( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){   //コピー
+       array2[i] = array1[i]; 
+    }
+
+    for( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){   //数値表示
+        printf("array2[%d] = %d\n", i, array2[i]);
+    }
+
+    return 0;
 }
