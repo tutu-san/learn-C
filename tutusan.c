7afd35e08208df63e70d5120e85b6fce678eeef1
tutu_san <tumuri4071.dev@gmail.com>
2022-06-16 19:54:30 +0900

    change code tutusan.c for 14-01-1

--- tutusan.c
+++ tutusan.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
-#include <memory.h>
 
 int main(void){
-    int array1[] = { 42, 79, 13, 19, 41 };
-    int array2[] = { 1, 2, 3, 4, 5 };
-    int i;
-
-    for ( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++ ){
-        printf("array2[%d] = %d\n", i, array2[i]);
-    }
-
-    memcpy( array2, array1, sizeof(array1)); //array1‚ğarray2‚ÉƒRƒs[
-
-    for ( i = 0; i < sizeof(array2) / sizeof(array2[0]); i++){
-        printf("array2[%d] = %d\n", i, array2[i]);
-    }
-    
+    char c = 'A';
+    printf("%c\n", c);
     return 0;
 }
