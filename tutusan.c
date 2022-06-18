b549d7fa372555f8fbaf4bf219659861d387d31a
tutu_san <tumuri4071.dev@gmail.com>
2022-06-18 17:17:25 +0900

    change code tutusan.c for 14-01-3

--- tutusan.c
+++ tutusan.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    char c = 'A' + 9; //0がスタートらしい。
-    printf("%c\n", c);
+    char c = '8'; //この8は数字として認識。
+    int suuti = c - '0'; //この時、数値に変換される。 //0は、48番。
+    printf("%d\n", suuti);
     return 0;
 }
