becef19ba0e2f953d8f398567c153fc0e85c5707
tutu_san <tumuri4071.dev@gmail.com>
2022-06-18 18:33:55 +0900

    change code tutusan.c for 14-02-3

--- tutusan.c
+++ tutusan.c
@@ -2,7 +2,13 @@
 
 int main(void){
 
-    char str[] = "MARIO";
+    char str[6];
+    str[0] = 'M';
+    str[1] = 'A';
+    str[2] = 'R';
+    str[3] = 'I';
+    str[4] = 'O';
+    str[5] = '\0';
     printf("%s\n", str);
     return 0;
 }
