0e21f518c7da6b5b81ac1308a1c34cccacdce593
tutu_san <tumuri4071.dev@gmail.com>
2022-06-19 23:04:13 +0900

    change code tutusan.c for 14-03-3

--- tutusan.c
+++ tutusan.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void){
-    char str1[] = "MARIO", str2[10];
-    strncpy(str2, str1 , 3);
-    str2[3] = '\0'; //EOS‚ð’Ç‰Á
-    printf("%s\n", str2); 
+    char str[] = "DRAGON""QUEST";
+    printf("%s\n", str);
     return 0;
 }
