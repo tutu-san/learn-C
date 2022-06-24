1ea6c37609f7ce77d17f956dc39d84e77f4020ef
tutu_san <tumuri4071.dev@gmail.com>
2022-06-24 17:29:48 +0900

    change code tutusan.c for 14-03-12

--- tutusan.c
+++ tutusan.c
@@ -2,18 +2,11 @@
 #include <string.h>
 
 int main(void){
-
-    int len, i;
-    char str1[256], str2 [] = "DRAGONQUEST";
+    char str1[256], str2[] = "DRAGONQUEST";
 
     scanf("%s", str1);
-    len = strlen(str2);
 
-    for (i = 0; i < len + 1; i++){
-        if (str1[i] != str2[i]) break; //ÅŒã‚Ü‚Å‡‚í‚È‚¯‚ê‚ÎA“r’†‚Åfor•¶‚©‚ç”²‚¯‚éB
-    }
-
-    if ( i == len +1){
+    if ( strcmp(str1, str2 ) == 0){
         printf("“¯‚¶\n");
     }else{
         printf("ˆá‚¤I\n");
