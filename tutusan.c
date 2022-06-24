6078d0bfc38851767fecb43c3c0a4d0b7c832f62
tutu_san <tumuri4071.dev@gmail.com>
2022-06-24 16:57:24 +0900

    change code tutusan.c for 14-03-7

--- tutusan.c
+++ tutusan.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-    char str[16];
-    char str1[12] = "DRAGON";
-    char str2[] = "QUEST";
-    int i = 3;
-    sprintf(str, "%s%s%d\n", str1, str2 , i);
-    printf(str);
+    char str[32];
+    scanf("%s", str);
+    printf("%s\n", str);
     return 0;
 }
