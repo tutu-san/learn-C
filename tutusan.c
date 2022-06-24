bd4edb2bd259ed230803ee870aecc263172542cb
tutu_san <tumuri4071.dev@gmail.com>
2022-06-24 17:13:40 +0900

    change code tutusan.c for 14-03-10

--- tutusan.c
+++ tutusan.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
     int i;
 
     char str[256];
     scanf("%s", str);
-    for ( i = 0; str[i] != '\0'; i++);
+
+    i = strlen(str);
 
     printf("%d\n", i);
 
