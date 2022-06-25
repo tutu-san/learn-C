38f48aed2ed101842c1834c0cb85b7c690e688dd
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 10:57:40 +0900

    change code tutusan.c for 15-05-02

--- tutusan.c
+++ tutusan.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void){
+
     int *p;
     int i;
     p = &i;
-    printf("p = %p\n", p);
-    printf("&i = %p\n", &i);
+    *p = 10; //’Êí•Ï”ƒ‚[ƒh‚É‚È‚Á‚½P‚É10‚ğ‚¢‚ê‚é
+    printf("*p = %d\n", *p);
+    printf("i = %d\n", i);
     return 0;
 }
