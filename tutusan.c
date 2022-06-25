9790c173d013b2548d96ce7a457c53eeb25a4856
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 13:27:43 +0900

    change code tutusan.c for 15-06-04

--- tutusan.c
+++ tutusan.c
@@ -4,8 +4,10 @@ int getaverage( int data[10]);
 
 int main(void){
 
-    int average , array[5] = { 15, 78, 98, 15, 98 };
+    int average , array[10] = { 15, 78, 98, 15, 98, 85, 17, 35, 42, 15 };
+    printf("array[3] = %d\n", array[3]);
     average = getaverage(array); //配列名を送ることは、アドレスを送ること？
+    printf("array[3] = %d\n", array[3]);
     printf("%d\n", average);
     return 0;
 }
@@ -16,5 +18,6 @@ int getaverage(int data[10]){
     for (i = 0; i < 10; i++){
         average += data[i];   //10この合計を出す。
     }
+    data[3] = 111; //引数の配列の値を変更する。　　参照したarrayに影響を及ぼす。
     return average / 10; //10で割って、平均値を返す。
 }
