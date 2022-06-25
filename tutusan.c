46a135ddec7ceff95d0e2fef9d7ec0fed3e5a73e
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 10:27:54 +0900

    change code tutusan.c for 15-03-02

--- tutusan.c
+++ tutusan.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main(void){
-    int array[10];
-    printf("array___(%p)\n", array);
-    printf("array[0](%p)\n", &array[0]);
-    printf("array[1](%p)\n", &array[1]);
-    printf("array[2](%p)\n", &array[2]);
-    return 0;
+    char str[256] = "Miss_Kobasashi's_";
+    scanf("%s", &str[17]); //17”Ô‚ÌƒAƒhƒŒƒX)
+    printf("%s\n", str);
 }
