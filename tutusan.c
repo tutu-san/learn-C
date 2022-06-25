9a83a98a98946f3076e3fa2960d97d3b1b25be93
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 13:38:35 +0900

    change code tutusan.c for 15-06-04-02

--- tutusan.c
+++ tutusan.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int getaverage( int data[10]);
+int getaverage( int *data);
 
 int main(void){
 
     int average , array[10] = { 15, 78, 98, 15, 98, 85, 17, 35, 42, 15 };
     printf("array[3] = %d\n", array[3]);
-    average = getaverage(array); //配列名を送ることは、アドレスを送ること？
+    average = getaverage(array); //配列名を送ることは、アドレスを送ることだった。
     printf("array[3] = %d\n", array[3]);
     printf("%d\n", average);
     return 0;
 }
 
-int getaverage(int data[10]){ 
+int getaverage(int *data){ 
 
     int i, average = 0;
     for (i = 0; i < 10; i++){
