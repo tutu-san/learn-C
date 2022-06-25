3672c3046638c5a6b8107e2abec6509a3c1164cf
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 10:12:54 +0900

    change code tutusan.c for 15-02-02

--- tutusan.c
+++ tutusan.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    int i;
-    printf("%p\n", &i);
+    int array[10];
+    printf("array___(%p)\n", array);
+    printf("array[0](%p)\n", &array[0]);
+    printf("array[1](%p)\n", &array[1]);
+    printf("array[2](%p)\n", &array[2]);
     return 0;
 }
