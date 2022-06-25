f77550312c39fcc0e5cd9982fd9ee747c42e45f0
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 10:04:23 +0900

    change code tutusan.c for 15-02-01

--- tutusan.c
+++ tutusan.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void){
-    char str1[256], str2[] = "DRAGONQUEST";
-
-    scanf("%s", str1);
-
-    if ( strcmp(str1, str2 ) == 0){
-        printf("ìØÇ∂\n");
-    }else{
-        printf("à·Ç§ÅI\n");
-    }
-
+    int i;
+    printf("%p\n", &i);
     return 0;
 }
