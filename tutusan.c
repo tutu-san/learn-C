387334793337c547c8b0db1f9a2dc9e8dc320c0b
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 14:05:26 +0900

    15-07-01

--- tutusan.c
+++ tutusan.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-int getaverage( int data[]);
-
 int main(void){
+    int *data;
+    int average = 0, array[10] = {15, 78, 98, 15, 98, 85, 17, 35, 42, 15};
 
-    int average , array[10] = { 15, 78, 98, 15, 98, 85, 17, 35, 42, 15 };
-    printf("array[3] = %d\n", array[3]);
-    average = getaverage(array); //配列名を送ることは、アドレスを送ることだった。
-    printf("array[3] = %d\n", array[3]);
-    printf("%d\n", average);
-    return 0;
-}
+    data = array; //ポインタ変数に配列のアドレスを代入
 
-int getaverage(int data[]){ 
-
-    int i, average = 0;
-    for (i = 0; i < 10; i++){
-        average += data[i];   //10この合計を出す。
+    for (int i = 0; i < 10; i++){
+        average += data[i]; //配列みたいに使える。 array[i]って書いてるのとおなじ？
     }
-    data[3] = 111; //引数の配列の値を変更する。　　参照したarrayに影響を及ぼす。
-    return average / 10; //10で割って、平均値を返す。
+
+    printf("%d\n", average / 10);
+    return 0;
 }
