136ba4e2b02b2cdd30210c40828ebb796c8dee3b
tutu_san <tumuri4071.dev@gmail.com>
2022-06-25 10:47:21 +0900

    change code tutusan.c for 15-05-01

--- tutusan.c
+++ tutusan.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    char str[256] = "Miss_Kobasashi's_";
-    scanf("%s", &str[17]); //17”Ô‚ÌƒAƒhƒŒƒX)
-    printf("%s\n", str);
+    int *p;
+    int i;
+    p = &i;
+    printf("p = %p\n", p);
+    printf("&i = %p\n", &i);
+    return 0;
 }
