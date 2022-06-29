31d5d958bdc264b65585543c53e8b1c8969fd524
tutu_san <tumuri4071.dev@gmail.com>
2022-06-30 01:26:22 +0900

    change code tutu15-q.c for 15-q unfinshed

--- tutu15-q.c
+++ tutu15-q.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int hantei (int number[], int *min, int *max);
+int hantei (int number[], int *min, int *max); //min max ポインタとして宣言
 
 int main(void){
     int i = 0;
     int number[9];
-    int *min, *max;
+    int min, max; //ここでは、ぽいんた変数にしないほうがいい？
     do{
         printf("Please Input Number %d times\n", i + 1);
         scanf("%d",&number[i]);
@@ -16,21 +16,19 @@ int main(void){
         }
     }while( number[i - 1] != -1 );
 
-    hantei(number, &min, &max);
+    hantei(number, &min, &max); //&で、アドレスを送る
 
     return 0;
 }
 
-int hantei(int number[], int *min, int *max){
+int hantei(int number[], int *min, int *max){ //アドレスをもらうのでポインタ型 numberは、配列をもらうため。
     
     for ( int i = 0; i < 10; i++){
         if(number[i] != -1){
-            if( number[i] < min) min = number[i];
-            if( number[i] > max) max = number[i];
+            if( number[i] < *min) *min = number[i];
+            if( number[i] > *max) *max = number[i];
         }
         if(number[i] = -1) break;
     }
-
-    
 
 }
