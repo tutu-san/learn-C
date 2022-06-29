2bc290e57804d3990d740462adce11a5dc6adbf9
tutu_san <tumuri4071.dev@gmail.com>
2022-06-30 01:47:15 +0900

    tutu15-q.c has bugs to fix

--- tutu15-q.c
+++ tutu15-q.c
@@ -5,7 +5,8 @@ int hantei (int number[], int *min, int *max); //min max ポインタとして宣言
 int main(void){
     int i = 0;
     int number[9];
-    int min, max; //ここでは、ぽいんた変数にしないほうがいい？
+    int min, max; //ここでは、ぽいんた変数にした
+    int degug;
     do{
         printf("Please Input Number %d times\n", i + 1);
         scanf("%d",&number[i]);
@@ -16,9 +17,9 @@ int main(void){
         }
     }while( number[i - 1] != -1 );
 
-    hantei(number, &min, &max); //&で、アドレスを送る
+    degug = hantei(number, &min, &max); //&はいらない
 
-    printf("%d\n", min);//結果表示
+    printf("%d\n", min);//結果表示　通常変数モード
     printf("%d\n", max);
 
     return 0;
@@ -28,10 +29,10 @@ int hantei(int number[], int *min, int *max){ //アドレスをもらうのでポインタ型 n
     
     for ( int i = 0; i < 10; i++){
         if(number[i] != -1){
-            if( number[i] < *min) *min = number[i];
-            if( number[i] > *max) *max = number[i];
+            if( number[i] < *min) number[i] = *min;
+            if( number[i] > *max) number[i] = *max;
         }
         if(number[i] = -1) break;
     }
-
+    return 0;
 }
