125020374000643458e248147b119b951d24aca2
tutu_san <tumuri4071.dev@gmail.com>
2022-07-01 18:04:44 +0900

    change code tutusan.c for 16-01-04

--- tutusan.c
+++ tutusan.c
@@ -11,13 +11,25 @@ struct student{         //構造体タグは、関数よりも先に宣言する。
 };
 
 int main(void){
-    struct student data;
+    struct student data1, data2;
 
-    data.year = 10;
-    printf("%d", data.year); //それぞれの要素には、"."で、アクセス。
+    data1.year = 3;
+    data1.clas = 4;
+    data1.number = 18;
+    strcpy(data1.name, "MARIO");
+    data1.stature = 168.2;
+    data1.weight = 72.4;
 
-    strcpy(data.name, "MARIO");
-    printf("%d", data.name);
+    data2 = data1; //data1の内容はdata2へコピー
+
+    //data1&data2の情報全表示
+    printf("data1.year = %d : data1.year = %d\n", data1.year, data2.year);
+    printf("data1.clas = %d : data2.clas = %d\n", data1.clas, data2.clas);
+    printf("data1.number = %d : data2.number = %d\n", data1.number, data2.number);
+    printf("data1.name = %s : data2.name %s\n", data1.name, data2.name);
+    printf("data1.stature = %f : data2.stature = %f\n", data1.stature, data2.stature);
+    printf("data1.weight = %f : data2.weight = %f", data1.weight, data2.weight);
+
 
     return 0;
 }
