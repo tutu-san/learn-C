30d8e2229bbf14324eeb42086b46ae79a9a47121
tutu_san <tumuri4071.dev@gmail.com>
2022-07-01 17:17:52 +0900

    change code tutusan.c for 16-01-01

--- tutusan.c
+++ tutusan.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
+struct student{         //構造体タグは、関数よりも先に宣言する。
+    int year;
+    int clas;
+    int number;
+    char name[64];
+    double stature;
+    double weight;
+};
+
 int main(void){
-    int *data;
-    int average = 0;
-    int array[10] = {15, 78, 98, 15, 98, 85, 17, 35, 42, 15};
-
-    for (data = array; data != &array[10]; data ++ ){ //配列のアドレスをdataに入れる。 そして、data内のアドレスを一つずつ増やす。
-        //ここに注目!
-        average += *data; // アドレスから、配列にアクセスしている？
-    }
-
-    printf("%d\n", average / 10);
-
+    struct student data;
     return 0;
 }
